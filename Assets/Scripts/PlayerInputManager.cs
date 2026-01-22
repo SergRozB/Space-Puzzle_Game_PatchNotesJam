bedeb02fe6c46705fa0773165b96efa06895e9a8
SergRozB <115506557+SergRozB@users.noreply.github.com>
2026-01-22 19:51:27 +0000

    actual last push before merge

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -10,18 +10,24 @@ public class PlayerInputManager : MonoBehaviour
 {
     public InputActionReference pause;
     public InputActionReference move;
+    [SerializeField] private InputActionReference scroll;
     public bool isPaused = false;
     [SerializeField] private GameObject[] inventory;
     [SerializeField] private bool isInventoryFull = false;
     [SerializeField] private int inventorySize = 8;
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private GameObject inventorySlot;
+    [SerializeField] private GameObject inventorySlotPrefab;
     [SerializeField] private Transform leftInventoryTransform;
     [SerializeField] private Transform rightInventoryTransform;
     [SerializeField] private Transform inventorySlotsParent;
     private GameObject[] inventorySlotGameObjects;
     private float inventorySideCushion = 50f;
-    [SerializeField] private Sprite selectedItemSlotSprite; 
+    [SerializeField] private Sprite selectedItemSlotSprite;
+    [SerializeField] private Sprite noItemInSlotSprite;
+    [SerializeField] private int selectedSlot = 0;
+    private bool changedSlot = false;
+    [SerializeField] private GameObject selectedItem;
+    [SerializeField] private float itemSpriteScale = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,18 +42,23 @@ public class PlayerInputManager : MonoBehaviour
     {
         isPaused = pause.action.triggered ? !isPaused : isPaused;
         transform.Translate(move.action.ReadValue<Vector2>() * Time.deltaTime * moveSpeed);
+        SelectInventorySlot();
     }
 
     private void SetInventorySlots() 
     {
         float xDifference = rightInventoryTransform.position.x - leftInventoryTransform.position.x;
-        float inventorySlotWidth = inventorySlot.transform.localScale.x * 100f;
+        float inventorySlotWidth = inventorySlotPrefab.transform.localScale.x * 100f;
         float distanceBetween = (xDifference - (inventorySideCushion*2) - inventorySlotWidth) / (inventorySize-1);
         float startingCushion = inventorySideCushion + (inventorySlotWidth/2);
+        float yChange = 85f;
         for (int i = 0; i < inventorySize; i++)
         {
-            Vector3 slotPosition = leftInventoryTransform.position + new Vector3(startingCushion+distanceBetween * (i), 0, 0);
-            inventorySlotGameObjects[i] = Instantiate(inventorySlot, slotPosition, Quaternion.identity, inventorySlotsParent);
+            Vector3 slotPosition = leftInventoryTransform.position + new Vector3(startingCushion+distanceBetween * (i), yChange, 0);
+            inventorySlotGameObjects[i] = Instantiate(inventorySlotPrefab, slotPosition, Quaternion.identity, inventorySlotsParent);
+            UnityEngine.UI.Image slotImage = inventorySlotGameObjects[i].GetComponent<UnityEngine.UI.Image>();
+            slotImage.sprite = noItemInSlotSprite;
+            slotImage.rectTransform.localScale = new Vector3(itemSpriteScale, itemSpriteScale, 0);
         }
     }
 
@@ -74,15 +85,53 @@ public class PlayerInputManager : MonoBehaviour
         {
             if(i == index) 
             {
-                GameObject itemInInventory = Instantiate(item, inventorySlotGameObjects[i].transform.position, Quaternion.identity, inventorySlotGameObjects[i].transform);
+                GameObject itemInInventory = Instantiate(item, inventorySlotGameObjects[i].transform.position, Quaternion.identity, inventorySlotsParent);
                 itemInInventory.transform.localScale = new Vector3(50f, 50f, 0);
                 itemInInventory.AddComponent<UnityEngine.UI.Image>();
                 UnityEngine.UI.Image image = itemInInventory.GetComponent<UnityEngine.UI.Image>();
                 image.sprite = item.GetComponent<SpriteRenderer>().sprite;
-
+                image.SetNativeSize();
+                image.rectTransform.localScale = new Vector3(itemSpriteScale/2, itemSpriteScale/2, 0);
             }
         }
     }
+
+    private void SelectInventorySlot() 
+    {
+        if(scroll.action.ReadValue<float>() > 0) 
+        {
+            selectedSlot = (selectedSlot + 1) % inventorySize;
+            changedSlot = true;
+        }
+
+        else if(scroll.action.ReadValue<float>() < 0) 
+        {
+            selectedSlot = (selectedSlot - 1 + inventorySize) % inventorySize;
+            changedSlot = true;
+        }
+
+        if(changedSlot) 
+        {
+            for (int i = 0; i < inventorySize; i++)
+            {
+                UnityEngine.UI.Image slotImage = inventorySlotGameObjects[i].GetComponent<UnityEngine.UI.Image>();
+                if (i == selectedSlot)
+                {
+                    selectedItem = inventory[i];
+                    slotImage.sprite = selectedItemSlotSprite;
+                    slotImage.rectTransform.localScale = new Vector3(itemSpriteScale, itemSpriteScale, 0);
+                }
+                else
+                {
+                    slotImage.sprite = noItemInSlotSprite;
+                    slotImage.rectTransform.localScale = new Vector3(itemSpriteScale, itemSpriteScale, 0);
+                }
+            }
+            changedSlot = false;
+        }
+    }
+
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
